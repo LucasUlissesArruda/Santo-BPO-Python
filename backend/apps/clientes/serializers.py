e0fb0cfadf10e0600da53022c9edeea6fcc0eedc
Lucas Arruda <168601194+LucasUlissesArruda@users.noreply.github.com>
2025-10-27 16:42:50 -0300

    (fix): deu tudo errado

--- backend/apps/clientes/serializers.py
+++ backend/apps/clientes/serializers.py
@@ -1,1 +1,8 @@
+# backend/apps/clientes/serializers.py
+from rest_framework import serializers
+from .models import Cliente
 
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = '__all__' # Expõe todos os campos
