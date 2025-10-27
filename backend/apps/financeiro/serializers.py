2da0cb13200433aaefe1e1e6928f7779ec549739
LipeProgramer <felipe.156888-2023@aluno.unicv.edu.br>
2025-10-26 21:04:35 -0300

    mais ajustes

--- backend/apps/financeiro/serializers.py
+++ backend/apps/financeiro/serializers.py
@@ -1,1 +1,14 @@
+# financeiro/serializers.py
 
+from rest_framework import serializers
+from .models import Cliente, Lancamento 
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+
+class LancamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lancamento
+        fields = '__all__'
